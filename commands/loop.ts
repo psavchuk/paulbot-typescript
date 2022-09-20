b80aea3750a32824dacc7cd39b9576d5b623002c
psavchuk <psavchuk22@gmail.com>
2022-09-20 13:51:37 -0400

    Added missing loop interaction

---
+++ commands/loop.ts
@@ -0,0 +1,44 @@
+import { ButtonStyle } from "discord.js";
+import { bot } from "..";
+import { updateInteraction, updateMessageRowEmbedButton } from "../common/embed-functions";
+import { loopButton } from "../models/bot.constants";
+
+export default {
+    name: "loop",
+    description: "Loops the current song",
+    async execute(interaction?: any, deferReply: boolean = false, guildId?: string) {
+
+        const id = interaction?.guildId || guildId;
+        const connection = bot.connections.get(id);
+
+        if(!connection) return;
+
+        if(connection.playerState.isLooping === false) {
+            updateMessageRowEmbedButton(
+                connection.messageState.messageRows[loopButton.row].components[loopButton.id],
+                "End Loop",
+                ButtonStyle.Success,
+                false
+            );
+            connection.playerState.isLooping = true;
+        }
+        else {
+            updateMessageRowEmbedButton(
+                connection.messageState.messageRows[loopButton.row].components[loopButton.id],
+                "Start Loop",
+                ButtonStyle.Secondary,
+                false
+            );
+            connection.playerState.isLooping = false;
+        }
+
+        if(interaction) {
+            await updateInteraction(connection, interaction);
+        }
+
+        // if(interaction && deferReply) {
+        //     await interaction?.deferReply();
+        //     await interaction?.deleteReply();
+        // }
+    }
+}
