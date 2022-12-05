593941e7648c1031218489a274674615f5f04ad7
psavchuk <psavchuk22@gmail.com>
2022-12-05 13:54:38 -0500

    Skip command fixes and cleanup

--- commands/skip.ts
+++ commands/skip.ts
@@ -1,7 +1,7 @@
 import { bot } from "..";
 import { autoplaySong } from "../common/autoplay-functions";
 import { playSong } from "../common/play-song-functions";
-import { AutoplayType } from "../models/bot.interfaces";
+import { AutoplayType, ISong } from "../models/bot.interfaces";
 
 export default {
     name: "skip",
@@ -18,31 +18,33 @@ export default {
 
         const playerState = connection.playerState;
 
+        let song: ISong;
+
         if(playerState.currentSong?.chapters?.length > 0) {
             if(++playerState.currentSong.currentChapter < playerState.currentSong.chapters.length) {
-                await playSong(id, playerState.currentSong);
-                return;
+                song = playerState.currentSong;
+            }
+        }
+        else {
+            if(playerState.queue.length === 0) {
+                if(playerState.autoplayer.enabled) {
+                    if(playerState.autoplayer.queue.length <= 1) 
+                        await autoplaySong(id, playerState.currentSong, AutoplayType.youtubeMix);
+    
+                    song = playerState.autoplayer.queue.shift();
+                }
+            }
+            else
+            {
+                song = playerState.queue.shift();
+                if(song.chapters?.length > 0)
+                    song.currentChapter++;
             }
         }
 
-        if(playerState.queue.length === 0) {
-            if(playerState.autoplayer.enabled) {
-                if(playerState.autoplayer.queue.length <= 1)
-                    await autoplaySong(id, playerState.currentSong, AutoplayType.youtubeMix);
-
-                const _song = playerState.autoplayer.queue.shift();
-                await playSong(id, _song);
-                return;
-            }
-        }
-        else
-        {
-            const _song = playerState.queue.shift();
-            if(_song.chapters?.length > 0)
-                _song.currentChapter++;
-
-            await playSong(id, _song);
-            return;
+        if(song) {
+            connection.playerState.player.unpause();
+            await playSong(id, song);
         }
     }
 }
